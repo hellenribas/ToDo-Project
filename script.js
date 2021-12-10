41b28b622bc83ac6935a1271deea897de411bf7b
hellenribas <tchwtrabalho@gmail.com>
2021-12-10 20:59:28 -0300

    requisitos 10 e 11

--- script.js
+++ script.js
@@ -54,11 +54,52 @@ function addCor(event) {
 liAdicionadas.addEventListener('dblclick', riscandoTexto);
 
 function riscandoTexto(event3) {
-    console.log('entrounafunçãorisco');
     if (event3.target.classList[1] !== 'completed') {
         event3.target.classList.add('completed');
     } else if (event3.target.classList[1] === 'completed') {
         console.log('entrou no if else risco');
         event3.target.classList.remove('completed');
+    }
+}
+let botoes = document.getElementById('botoes');
+function criarBotaoRemover() {
+    let botao2 = document.createElement('button');
+    botao2.id = 'apaga-tudo';
+    let text2 = document.createTextNode('Apagar');
+    botao2.appendChild(text2);
+    // botao.style.backgroundColor = 'green';
+    botoes.appendChild(botao2);
+}
+criarBotaoRemover();
+let li = document.getElementsByClassName('liAdd');
+let botaoRemove = document.getElementById('apaga-tudo');
+let ol = document.querySelector('#lista-tarefas');
+botaoRemove.addEventListener('click', removerLista);
+function removerLista() {
+    let liIndex = 0;
+    const olLength = ol.children.length
+    for (index = olLength - 1; index >= 0; index -= 1) {
+        liIndex = li[index];
+        ol.removeChild(liIndex);
+    }
+}
+function criarBotaoRemoverFinalizado() {
+    let botao3 = document.createElement('button');
+    botao3.id = 'remover-finalizados';
+    let text3 = document.createTextNode('Apagar Finalizados');
+    botao3.appendChild(text3);
+    // botao.style.backgroundColor = 'green';
+    botoes.appendChild(botao3);
+}
+criarBotaoRemoverFinalizado()
+let botao3 = document.getElementById('remover-finalizados');
+botao3.addEventListener('click', removerFinalizados);
+function removerFinalizados() {
+    const olLength = ol.children.length
+    for (index = olLength - 1; index >= 0; index -= 1) {
+        if (ol.children[index].classList[1] === 'completed') {
+            liIndex = li[index];
+            ol.removeChild(liIndex);
+        }
     }
 }
